3819e6fc674db9fdbe0504328157282cba52f8e5
petinexus <57102986+petinexus@users.noreply.github.com>
2022-01-22 19:27:40 +0100

    Simple Get

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService){}
+    
+    @Get()
+    async getUsers(){
+        return this.userService.getUsers();
+    }
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { USERS } from './user.mock';
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    private users = USERS;
+
+    public async getUsers(){
+        var json = JSON.stringify(this.users);
+        return json;
+    }
+}
+
+
