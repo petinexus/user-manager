7c81379c3a38192b45ade34f06885a43bdebeb41
petinexus <57102986+petinexus@users.noreply.github.com>
2022-01-22 19:01:43 +0100

    Make own Service layer and simple DB

---
+++ src/user/user.mock.ts
@@ -0,0 +1,26 @@
+export const USERS = [
+    {
+        id: '1',
+        name: 'Jack',
+        age: '19',
+        gender: 'man',
+    },
+    {
+        id: '2',
+        name: 'Johnny',
+        age: '20',
+        gender: 'man',
+    },
+    {
+        id: '3',
+        name: 'Jim',
+        age: '22',
+        gender: 'man',
+    },
+    {
+        id: '4',
+        name: 'Jameson',
+        age: '30',
+        gender: 'man',
+    },
+]
